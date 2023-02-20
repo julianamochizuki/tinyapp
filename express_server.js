f69046d197ebb306d127afadde06206c37abb543
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-20 15:35:38 -0800

    Create URL submission form

--- express_server.js
+++ express_server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = "";
+  for (let i = 0; i <= 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
+
+app.set("view engine", "ejs");
+
+// converting the request body from a Buffer into string
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// adding additional routes
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -22,6 +32,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
