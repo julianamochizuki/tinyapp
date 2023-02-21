68bdcaeadc51a180cf65dfaffc8cba98b18dc6b8
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-20 19:37:50 -0800

    Update URLs

--- express_server.js
+++ express_server.js
@@ -27,29 +27,31 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// passing the URL data to the template
+// Passes the URL data to the template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Page to create new tinyURL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// form submission and redirection
+// Submits new URL
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  // console.log(req.body);
   urlDatabase[shortURL] = req.body["longURL"];
   res.redirect(`/urls/${shortURL}`);
 });
 
+// Shows generated tinyURL
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
 
+// Accesses URL page through tinyURL
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   if (!(req.params.id in urlDatabase)) {
@@ -58,8 +60,20 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+// Deletes URLs
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
+  res.redirect(`/urls`);
+});
+
+// Redirects to the page to update URL
+app.post("/urls/:id/edit", (req, res) => {
+  res.redirect(`/urls/${req.params.id}`);
+});
+
+// Updates URLs
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body["longURL"];
   res.redirect(`/urls`);
 });
 
