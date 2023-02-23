8f5d261dc30240be3ac2c60680078470d9f29064
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-22 16:48:44 -0800

    Update /edit and /delete endpoints

--- express_server.js
+++ express_server.js
@@ -36,13 +36,25 @@ const generateRandomString = function() {
   return randomString;
 };
 
-const getUserByEmail = email => {
+const getUserByEmail = function(email) {
   for (const key in users) {
     if (users[key]["email"] === email) {
       return users[key];
     }
   }
   return null;
+};
+
+const urlsForUser = function(id) {
+  let result = {};
+  for (const key in urlDatabase) {
+    if (urlDatabase[key].userId === id) {
+      result[key] = {
+        longURL: urlDatabase[key].longURL
+      };
+    }
+  }
+  return result;
 };
 
 app.set("view engine", "ejs");
@@ -61,9 +73,12 @@ app.get("/urls.json", (req, res) => {
 // Passes the URL data to the template
 app.get("/urls", (req, res) => {
   const userId = req.cookies["user_id"];
+  if (!userId) {
+    return res.send("<html><body><h2>Please login or register first.</h2></body></html>");
+  }
   const templateVars = {
     user: users[userId],
-    urls: urlDatabase
+    urls: urlsForUser(userId)
   };
   res.render("urls_index", templateVars);
 });
@@ -97,6 +112,12 @@ app.post("/urls", (req, res) => {
 // Shows generated tinyURL
 app.get("/urls/:id", (req, res) => {
   const userId = req.cookies["user_id"];
+  if (!userId) {
+    return res.send("<html><body><h2>Please login or register first.</h2></body></html>");
+  }
+  if (!(req.params.id in urlsForUser(userId))) {
+    return res.status(404).send("<html><body><h2>Page not found.\n</h2><h3>The requested URL page was not found on this server.</h3></body></html>\n");
+  }
   const templateVars = {
     user: users[userId],
     id: req.params.id,
@@ -107,7 +128,11 @@ app.get("/urls/:id", (req, res) => {
 
 // Accesses URL page through tinyURL
 app.get("/u/:id", (req, res) => {
-  if (!(req.params.id in urlDatabase)) {
+  const userId = req.cookies["user_id"];
+  if (!userId) {
+    return res.send("<html><body><h2>Please login or register first.</h2></body></html>");
+  }
+  if (!(req.params.id in urlsForUser(userId))) {
     return res.status(404).send("<html><body><h2>Page not found.\n</h2><h3>The requested URL page was not found on this server.</h3></body></html>\n");
   }
   const longURL = urlDatabase[req.params.id]["longURL"];
@@ -116,12 +141,26 @@ app.get("/u/:id", (req, res) => {
 
 // Deletes URLs
 app.post("/urls/:id/delete", (req, res) => {
+  const userId = req.cookies["user_id"];
+  if (!userId) {
+    return res.send("<html><body><h2>Please login or register first.</h2></body></html>");
+  }
+  if (!(req.params.id in urlsForUser(userId))) {
+    return res.status(404).send("<html><body><h2>ShortURL does not exist.\n</h2></body></html>\n");
+  }
   delete urlDatabase[req.params.id];
   res.redirect(`/urls`);
 });
 
 // Redirects to the page to update URL
 app.post("/urls/:id/edit", (req, res) => {
+  const userId = req.cookies["user_id"];
+  if (!userId) {
+    return res.send("<html><body><h2>Please login or register first.</h2></body></html>");
+  }
+  if (!(req.params.id in urlsForUser(userId))) {
+    return res.status(404).send("<html><body><h2>ShortURL does not exist.\n</h2></body></html>\n");
+  }
   res.redirect(`/urls/${req.params.id}`);
 });
 
